988faba5b6da11abd7b183b0b95da5be742a0269
mengxifeng <zhanglinlinsd@163.com>
2014-03-11 10:41:55 +0800

    加入显示每个worker上task方法
    Signed-off-by: mengxifeng <zhanglinlinsd@163.com>

--- src/main/java/server/ShowStructure.java
+++ src/main/java/server/ShowStructure.java
@@ -11,30 +11,27 @@ import org.apache.zookeeper.Watcher;
 import common.util.LibraZKClient;
 import common.util.LibraZKPathUtil;
 
-
 /**
  * Created with IntelliJ IDEA.
- *
+ * 
  * @author :SDU.llzhang
- * @version 1.0.0
- *          Date: 14-1-17
- *          Time: 3:00PM
+ * @version 1.0.0 Date: 14-1-17 Time: 3:00PM
  */
 public class ShowStructure implements Watcher {
 
 	private String hosts;
 	private int sessionTimeout;
 	private LibraZKClient zkClient;
-	
+
 	private String root = LibraZKPathUtil.ROOT;
 	private List<String> allProjectsList;
 	private List<String> allWorkersList;
 	private List<String> myTasksList;
 	private List<String> myWorkersList;
+	private List<String> onWorkerTasksList;
 
 	public ShowStructure(String hosts, int sessionTimeout)
-			throws KeeperException, InterruptedException 
-	{
+			throws KeeperException, InterruptedException {
 		this.hosts = hosts;
 		this.sessionTimeout = sessionTimeout;
 		try {
@@ -45,14 +42,13 @@ public class ShowStructure implements Watcher {
 	}
 
 	private void start() throws IOException, KeeperException,
-			InterruptedException 
-	{
-		zkClient = new LibraZKClient(hosts, sessionTimeout, this);		
+			InterruptedException {
+		zkClient = new LibraZKClient(hosts, sessionTimeout, this);
+		System.out.println("OK*************");
 	}
 
 	private void readStructure(String path) throws KeeperException,
-			InterruptedException, IOException
-	{
+			InterruptedException, IOException {
 		if (path.endsWith("projects")) {
 			allProjectsList = zkClient.getChildren(path);
 		} else if (path.endsWith("all")) {
@@ -67,8 +63,7 @@ public class ShowStructure implements Watcher {
 	}
 
 	private void workerAndTaskPair(String path) throws KeeperException,
-			InterruptedException, IOException 
-	{
+			InterruptedException, IOException {
 		List<String> pathList = zkClient.getChildren(path);
 		if (!pathList.isEmpty()) {
 			for (String s : pathList) {
@@ -79,90 +74,95 @@ public class ShowStructure implements Watcher {
 				}
 			}
 		}
+
 	}
 
 	public List<String> getAllProjects() throws KeeperException,
-			InterruptedException, IOException
-	{
+			InterruptedException, IOException {
 		readStructure(root);
 		return allProjectsList;
 	}
 
 	public List<String> getAllWorkers() throws KeeperException,
-			InterruptedException, IOException 
-	{
+			InterruptedException, IOException {
 		readStructure(root);
 		return allWorkersList;
 	}
 
 	public List<String> getProjectTasks(String projectName)
-			throws KeeperException, InterruptedException, IOException 
-	{
+			throws KeeperException, InterruptedException, IOException {
 		workerAndTaskPair(LibraZKPathUtil.PROJECT_ROOT + "/" + projectName);
 		return myTasksList;
 	}
 
-	public List<String> getProjectWorkers(String projectName)
-			throws KeeperException, InterruptedException, IOException 
+	public List<String> getWorkerTasks(String workerName)
+			throws KeeperException, InterruptedException, IOException
 	{
+		String projectName = zkClient.checkAndGetDataString(LibraZKPathUtil
+				.genMyAllWorkerPath(workerName));
+		workerAndTaskPair(LibraZKPathUtil.PROJECT_ROOT + "/" + projectName);
+		for (String task : myTasksList) {
+			String workerId = zkClient.checkAndGetDataString(LibraZKPathUtil
+					.genSingleTaskPath(task, projectName));
+			if (workerId == workerName) {
+				onWorkerTasksList.add(task);
+			}
+		}
+		return onWorkerTasksList;
+	}
+
+	public List<String> getProjectWorkers(String projectName)
+			throws KeeperException, InterruptedException, IOException {
 		workerAndTaskPair(LibraZKPathUtil.PROJECT_ROOT + "/" + projectName);
 		return myWorkersList;
 	}
 
 	public boolean addProject(String projectName) throws KeeperException,
-			InterruptedException 
-	{
+			InterruptedException {
 		return zkClient.checkAndCreateNode(LibraZKPathUtil.PROJECT_ROOT + "/"
 				+ projectName, projectName.getBytes(), CreateMode.PERSISTENT);
 	}
 
 	public boolean deleteProject(String projectName) throws KeeperException,
-			InterruptedException 
-	{
+			InterruptedException {
 		return zkClient.checkAndDeleteNode(LibraZKPathUtil.PROJECT_ROOT + "/"
 				+ projectName);
 	}
 
 	public boolean addTask(String projectName, String taskName)
-			throws KeeperException, InterruptedException
-	{
+			throws KeeperException, InterruptedException {
 		return zkClient.checkAndCreateNode(LibraZKPathUtil
 				.genTaskRootPath(projectName)
 				+ "/" + taskName, new byte[0], CreateMode.PERSISTENT);
 	}
 
 	public boolean deleteTask(String projectName, String taskName)
-			throws KeeperException, InterruptedException 
-	{
+			throws KeeperException, InterruptedException {
 		return zkClient.checkAndDeleteNode(LibraZKPathUtil
 				.genTaskRootPath(projectName)
 				+ "/" + taskName);
 	}
 
 	public boolean assignWorker(String workerId, String projectName)
-			throws KeeperException, InterruptedException, IOException 
-	{
+			throws KeeperException, InterruptedException, IOException {
 		return zkClient.setDataString(LibraZKPathUtil
 				.genMyAllWorkerPath(workerId), projectName);
 	}
 
 	public boolean evacuateWorker(String workerId, String projectName)
-			throws KeeperException, InterruptedException 
-	{
+			throws KeeperException, InterruptedException {
 		return zkClient.setDataString(LibraZKPathUtil
 				.genMyAllWorkerPath(workerId), "");
 	}
 
 	@Override
-	public void process(WatchedEvent event) 
-	{
+	public void process(WatchedEvent event) {
 
 	}
 
 	public static void main(String[] args) throws IOException, KeeperException,
-			InterruptedException 
-	{
-		ShowStructure server = new ShowStructure("192.168.0.102:2181", 10000);
+			InterruptedException {
+		ShowStructure server = new ShowStructure("121.250.213.114:2181", 10000);
 
 		server.addProject("project1");
 		server.addTask("project1", "task1");
@@ -172,6 +172,9 @@ public class ShowStructure implements Watcher {
 		server.addProject("project2");
 		server.addTask("project2", "task2");
 		server.assignWorker("worker2", "project2");
+		for(String task:server.getWorkerTasks("worker1")){
+			System.out.println(task+",");
+		}
 
 		// server.evacuateWorker("worker1", "project1");
 		// server.evacuateWorker("worker2", "project2");
